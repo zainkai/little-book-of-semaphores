454ba7961950fc77faf2fccf5681ecef3bca95c1
Turkington, Kevin <kevin.turkington@nordstrom.com>
2019-10-28 10:59:32 -0700

    fixing mutex

--- chapter_3/3_4/mutex.go
+++ chapter_3/3_4/mutex.go
@@ -7,26 +7,14 @@ import (
 
 var wg = sync.WaitGroup{} // global wait group
 
-func threadA(mutex chan int) {
+func threadN(mutex chan int, name int) {
 	defer wg.Done()
 	for i := 0; i < 100; i++ {
 		count := <-mutex
 
 		count++
 
-		fmt.Println("threadA updated Count to: ", count)
-		mutex <- count
-	}
-}
-
-func threadB(mutex chan int) {
-	defer wg.Done()
-	for i := 0; i < 100; i++ {
-		count := <-mutex
-
-		count++
-
-		fmt.Println("threadB updated Count to: ", count)
+		fmt.Printf("thread_%d updated Count to: %d\n", name, count)
 		mutex <- count
 	}
 }
@@ -34,10 +22,11 @@ func threadB(mutex chan int) {
 func main() {
 	mu := make(chan int, 1)
 	mu <- 0
-	wg.Add(2)
 
-	go threadA(mu) // add 100 to count
-	go threadB(mu)
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go threadN(mu, i)
+	}
 
 	wg.Wait()
 }
